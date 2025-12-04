2bd6712642b8e90d5567af0bc114f955b8f32f7c
Glydel Mary Rose Visto <glydelmaryrose@gmail.com>
2025-12-04 08:49:40 +0800

    Add files via upload

---
+++ Number6.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Number6 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Number6
+{
+    public static void main(String [] args){
+    
+        int [] arr = {3, 8, 11, 14, 17};
+        int sum = 0, count = 0;
+        
+        for (int x : arr){
+            if (x % 2 != 0) {
+                sum += x;
+                count++;
+            }
+        }
+        System.out.println("Average of odd number = " + (sum / count));
+    }
+}
